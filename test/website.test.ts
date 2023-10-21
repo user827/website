dc5c2a44faa0bd0fe9a89c2893318da26efc1ead
user827 <user827@users.noreply.github.com>
2023-10-21 10:02:47 +0300

    Clean up

--- test/website.test.ts
+++ test/website.test.ts
@@ -3,9 +3,6 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { testEnv } from './env';
 import { WebsiteStage } from '../lib/website-stage';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/website-stack.ts
-
 const app = new cdk.App();
 const stage = new WebsiteStage(app, 'test', testEnv);
 
@@ -21,8 +18,4 @@ test('s3', () => {
 test('website', () => {
   const template = Template.fromStack(stage.website);
   expect(template.toJSON()).toMatchSnapshot();
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
 });
